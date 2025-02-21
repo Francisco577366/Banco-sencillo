bd0dd54ed80a11cb1fe0e93f4d89b02fdf8a129b
FranciscoNieto <108999735+Francisco577366@users.noreply.github.com>
2025-02-20 22:39:22 -0400

    Arreglos de decimales

--- script.js
+++ script.js
@@ -7,10 +7,22 @@
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
-  type: 'premium',
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -18,26 +30,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
-  type: 'standard',
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-  type: 'bad',
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-  type: 'bad',
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -86,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov}€</div>
+          <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
@@ -95,7 +103,7 @@ const displayMovements = function (movements, sort = false) {
 // Function que sumas todos los datos de transferencia
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance} EUR`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} EUR`;
 };
 
 // Function de suma para los datos de intereses, entrada,
@@ -103,22 +111,22 @@ const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const outCalc = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(outCalc)}€`;
+  labelSumOut.textContent = `${Math.abs(outCalc.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
+      console.log(int, arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 // Funcion para el login del nombre
@@ -151,7 +159,7 @@ btnLogin.addEventListener('click', function (e) {
   console.log(currentAccount);
 
   // Comprobamos si la contrasena en el input es igual a la de el objeto.
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     console.log('login');
 
     labelWelcome.textContent = `Welcome back, ${
@@ -172,7 +180,7 @@ btnLogin.addEventListener('click', function (e) {
 // Esta function transfiere los datos a otra cuenta.
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = Math.floor(inputTransferAmount.value);
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -196,7 +204,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     currentAccount.movements.push(amount);
@@ -211,7 +219,7 @@ btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
     currentAccount.username === inputCloseUsername.value &&
-    currentAccount.pin === Number(inputClosePin.value)
+    currentAccount.pin === +inputClosePin.value
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -309,7 +317,7 @@ const anyDeposits = movements.some(mov => mov > 1500);
 console.log(anyDeposits);
 
 console.log(movements.every(mov => mov > 0));
-console.log(account4.movements.every(mov => mov > 0));
+console.log(account2.movements.every(mov => mov > 0));
 
 const accountMovements = accounts
   .map(acc => acc.movements)
@@ -442,7 +450,9 @@ console.log(arr);
 const y = Array.from({ length: 7 }, () => 1);
 console.log(y);
 
-const z = Array.from({ length: 100 }, (_, i) => Math.random());
+const z = Array.from({ length: 100 }, (_, i) =>
+  Math.trunc(Math.random() * 100)
+);
 console.log(z);
 
 labelBalance.addEventListener('click', function () {
@@ -453,8 +463,10 @@ labelBalance.addEventListener('click', function () {
   console.log(movementsUI);
 });
 
-// Me devuelve los datos alrevez pero reverse muta el elemento origen
-console.log(movements);
++(
+  // Me devuelve los datos alrevez pero reverse muta el elemento origen
+  console.log(movements)
+);
 const reversedMov = movements.slice().reverse();
 console.log(movements);
 console.log(reversedMov);
@@ -560,3 +572,37 @@ console.log(dogEatingOkay);
 // 8
 const pepe = dogs.toSorted((a, b) => b.recFood - a.recFood);
 console.log(pepe);
+
+// ParseInt = convierte una string numerica en numero !parsefloat! = lo mismo pero con decimales
+// isNaN = te devuelve un valor booleano dependiendo si el elemento es nan o no !isFinite! = te devuelve un booleano si tiene un numero es true si no es false
+
+console.log(Number.isFinite(20));
+
+console.log(Math.sqrt(25));
+// Devuelve 25 dividido
+
+console.log(Math.max(1, 6, 9, 21, 12, 24));
+console.log(Math.min(1, 2, 4, 6, 8, 9, 12));
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomInt(10, 20));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+console.log(Math.trunc(23.3));
+console.log(Math.trunc(23.9));
+
+// Devuelve el dato con un arreglo decimal o completo.
+console.log((2.7).toFixed(0));
+console.log((2.345).toFixed(4));
+
+console.log(5 % 2);
